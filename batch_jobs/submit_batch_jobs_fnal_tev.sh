77fc2ca0807265a312cd954a5ea70ae2c8e943cd
Arbin Timilsina <arbin.timilsina@gmail.com>
2019-01-23 18:36:39 -0500

    bash_job script added for Fermilab

---
+++ batch_jobs/submit_batch_jobs_fnal_tev.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+## Set number of nodes for the job
+#SBATCH -N 1
+
+## Set number of tasks for the job
+#SBATCH -n 1
+
+## Specify to only use GPU nodes; no. per node
+##SBATCH --gres=gpu:1
+
+## Specify requested time
+## day-hr
+#SBATCH -t 7-00:00:00
+
+## Specify stdout, stderr log; default is slurm-jobid.out
+#SBATCH -o output.log
+#SBATCH -e error.log
+
+## Specify what to notify: BEGIN, END, FAIL...., ALL
+##SBATCH --mail-type=FAIL
+##SBATCH --mail-user arbint@bnl.gov
+
+echo ""
+echo "*********************************************************"
+echo "This was run on:"
+date
+echo "*********************************************************"
+echo ""
+## Invoking an interactive shell within singularity container
+singularity shell -nv DeepLearningWithProtoDUNE.img
+cd ../
+
+echo "*********************************************************"
+echo "Running python train_model.py"
+echo "JOB $SLURM_JOB_ID is running on $SLURM_JOB_NODELIST "
+echo "*********************************************************"
+echo ""
+srun python train_model.py -o Training -e Default
+
+echo "*********************************************************"
+echo "Running python analyze_model.py"
+echo "JOB $SLURM_JOB_ID is running on $SLURM_JOB_NODELIST "
+echo "*********************************************************"
+echo ""
+srun python analyze_model.py -p 5 -s Development
