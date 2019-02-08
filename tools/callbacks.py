cf3d26e1a48d0146fa26fe9cccc66452a11e0b28
Arbin Timilsina <arbin.timilsina@gmail.com>
2019-02-08 14:35:23 -0500

    Update callbacks.py

--- tools/callbacks.py
+++ tools/callbacks.py
@@ -4,23 +4,6 @@ from keras import backend as K
 from keras.callbacks import Callback
 from tools.data_tools import get_data_generator, preprocess_feature, preprocess_label
 
-def init_predictions():
-    config = configparser.ConfigParser()
-    config_path = os.path.join("configurations", "master_configuration.ini")
-    config.read(config_path)
-
-    IMAGE_WIDTH = int(config["DEFAULT"]["IMAGE_WIDTH"])
-    IMAGE_HEIGHT = int(config["DEFAULT"]["IMAGE_HEIGHT"])
-    IMAGE_DEPTH = int(config["DEFAULT"]["IMAGE_DEPTH"])
-    CLASS_NAMES = config["DEFAULT"]["CLASS_NAMES"].split()
-    FEATURE_FILE_TESTING = config["DEFAULT"]["FEATURE_FILE_TESTING"]
-    LABEL_FILE_TESTING = config["DEFAULT"]["LABEL_FILE_TESTING"]
-
-    generator_testing = get_data_generator(FEATURE_FILE_TESTING, LABEL_FILE_TESTING)
-    X, y = next(generator_testing)
-    X_preprocessed = preprocess_feature(X, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_DEPTH)
-    y_preprocessed = preprocess_label(y, IMAGE_WIDTH, IMAGE_HEIGHT, len(CLASS_NAMES))
-    y_preprocessed_max = K.argmax(y_preprocessed, axis=-1)
 
 class PredictionsCallback(Callback):
     def __init__(self, model):
@@ -29,13 +12,31 @@ class PredictionsCallback(Callback):
         self.prediction_image = []
         self.model = model
         init_predictions()
+        
+    def init_predictions(self):
+        config = configparser.ConfigParser()
+        config_path = os.path.join("configurations", "master_configuration.ini")
+        config.read(config_path)
 
+        IMAGE_WIDTH = int(config["DEFAULT"]["IMAGE_WIDTH"])
+        IMAGE_HEIGHT = int(config["DEFAULT"]["IMAGE_HEIGHT"])
+        IMAGE_DEPTH = int(config["DEFAULT"]["IMAGE_DEPTH"])
+        CLASS_NAMES = config["DEFAULT"]["CLASS_NAMES"].split()
+        FEATURE_FILE_TESTING = config["DEFAULT"]["FEATURE_FILE_TESTING"]
+        LABEL_FILE_TESTING = config["DEFAULT"]["LABEL_FILE_TESTING"]
+
+        generator_testing = get_data_generator(FEATURE_FILE_TESTING, LABEL_FILE_TESTING)
+        X, y = next(generator_testing)
+        y_preprocessed = preprocess_label(y, IMAGE_WIDTH, IMAGE_HEIGHT, len(CLASS_NAMES))
+        self.y_preprocessed_max = K.argmax(y_preprocessed, axis=-1)
+        self.X_preprocessed = preprocess_feature(X, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_DEPTH)
+    
     def on_epoch_end(self, epoch, logs=None):
-        prediction = self.model.predict_on_batch(X_preprocessed)
+        prediction = self.model.predict_on_batch(self.X_preprocessed)
         prediction_max = K.argmax(prediction, axis=-1)
 
-        self.feature_image.append(X_preprocessed.reshape(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_DEPTH))
-        self.label_image.append(y_preprocessed_max.reshape(IMAGE_WIDTH, IMAGE_HEIGHT))
+        self.feature_image.append(self.X_preprocessed.reshape(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_DEPTH))
+        self.label_image.append(self.y_preprocessed_max.reshape(IMAGE_WIDTH, IMAGE_HEIGHT))
         self.prediction_image.append(prediction_max.reshape(IMAGE_WIDTH, IMAGE_HEIGHT))
 
 class WeightsCallback(Callback):
